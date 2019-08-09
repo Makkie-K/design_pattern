d56b36c70cdc79ba081aae27856017b53e9c22cc
Makkie <hortensia221974@yahoo.co.jp>
2019-08-09 17:12:08 +0900

    upload all files

--- duck/Duck.java
+++ duck/Duck.java
@@ -1,2 +1,18 @@
-public class Duck {
+public abstract class Duck {
+	QuackBehavior quackBehavior;
+	FlyBehavior flyBehavior;
+	
+	public abstract void display();
+	
+	public void performQuack(){
+		quackBehavior.quick();
+	}
+	
+	public void performFly(){
+		flyBehavior.fly();
+	}
+	
+	public void swim(){
+		System.out.println("all duck float, even decoy!");
+	}
 }
