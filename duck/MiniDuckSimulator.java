3c11ecb2f06b04979faa74a16ab74ceb89b07c01
Makkie <hortensia221974@yahoo.co.jp>
2019-08-09 17:43:18 +0900

    MiniDuckSimulator.java modified

--- duck/MiniDuckSimulator.java
+++ duck/MiniDuckSimulator.java
@@ -1,6 +1,12 @@
 public class MiniDuckSimulator {
     public static void main(String[] args) {
+        System.out.println("Mallard Duck");
         Duck duck = new MallardDuck();
+        duck.performQuack();
+        duck.performFly();
+        
+        System.out.println("Model Duck");
+        duck = new ModelDuck();
         duck.performQuack();
         duck.performFly();
     }
