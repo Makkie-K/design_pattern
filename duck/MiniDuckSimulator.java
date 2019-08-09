e929bce0b8f3748eafd3a6f894a7f3dd2eac3753
Makkie <hortensia221974@yahoo.co.jp>
2019-08-09 18:15:27 +0900

    Main modified

--- duck/MiniDuckSimulator.java
+++ duck/MiniDuckSimulator.java
@@ -1,13 +1,24 @@
+import java.util.Scanner;
+
 public class MiniDuckSimulator {
     public static void main(String[] args) {
-        System.out.println("Mallard Duck");
-        Duck duck = new MallardDuck();
+        
+        System.out.println("1.Mallard Duck  2.Model Duck > ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Duck duck;
+        if(n == 1){
+            duck = new MallardDuck();
+        }else{
+            duck = new ModelDuck();
+        }
+        
         duck.performQuack();
         duck.performFly();
         
-        System.out.println("Model Duck");
-        duck = new ModelDuck();
-        duck.performQuack();
-        duck.performFly();
+//         System.out.println("Model Duck");
+//         duck = new ModelDuck();
+//         duck.performQuack();
+//         duck.performFly();
     }
 }
